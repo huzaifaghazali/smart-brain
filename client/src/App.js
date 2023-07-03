6f559461afbd6174eca4bf6f207f65466def2690
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-03 22:05:40 +0500

    Create components

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { Navigation, Logo, ImageLinkForm, FaceRecognition } from './components';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Smart Brain</h1>
-    </div>
-  );
+  return <div className='App'>
+    <Navigation />
+    <Logo />
+    <ImageLinkForm />
+    <FaceRecognition />
+  </div>;
 }
 
 export default App;
---
+++ client/src/components/index.js
@@ -0,0 +1,19 @@
+import FaceRecognition from './FaceRecognition/FaceRecognition';
+import HeaderLogo from './HeaderLogo/HeaderLogo';
+import ImageLinkForm from './ImageLinkForm/ImageLinkForm';
+import Logo from './Logo/Logo';
+import Navigation from './Navigation/Navigation';
+import Rank from './Rank/Rank';
+import Register from './Register/Register';
+import Signin from './Signin/Signin';
+
+export {
+  FaceRecognition,
+  HeaderLogo,
+  ImageLinkForm,
+  Logo,
+  Navigation,
+  Rank,
+  Register,
+  Signin,
+};
