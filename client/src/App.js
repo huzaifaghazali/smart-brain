ba8b9b011501c7667c936b463f79196633311aa8
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-03 23:31:50 +0500

    Add particles effect

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,18 @@
 import './App.css';
-import { Navigation, Logo, ImageLinkForm, FaceRecognition } from './components';
+import ParticlesBg from 'particles-bg';
 
+import { Navigation, Logo, ImageLinkForm, FaceRecognition } from './components';
 function App() {
-  return <div className='App'>
-    <Navigation />
-    <Logo />
-    {/* 
+  return (
+    <div className='App'>
+      <ParticlesBg  type="cobweb" bg={true} />
+      <Navigation />
+      <Logo />
+      {/* 
     <ImageLinkForm />
     <FaceRecognition /> */}
-  </div>;
+    </div>
+  );
 }
 
 export default App;
