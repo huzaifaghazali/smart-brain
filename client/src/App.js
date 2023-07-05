141b310942df8d32dd086908cd7592645c82a187
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-06 00:32:52 +0500

    Add favicon and set up the state

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,40 @@
+import { useState } from 'react';
 import ParticlesBg from 'particles-bg';
 import './App.css';
 
-import { Navigation, Logo, ImageLinkForm, Rank, FaceRecognition } from './components';
+import {
+  Navigation,
+  Logo,
+  ImageLinkForm,
+  Rank,
+  FaceRecognition,
+} from './components';
+
+const initialState = {
+  input: '',
+};
+
 function App() {
+  const [state, setState] = useState(initialState);
+
+  const onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    console.log('clicked');
+  };
+
   return (
     <div className='App'>
-      <ParticlesBg color='#ffffff'  type="cobweb" bg={true} />
+      <ParticlesBg color='#ffffff' type='cobweb' bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
       {/* 
     <FaceRecognition /> */}
     </div>
