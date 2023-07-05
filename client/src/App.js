c29febfd68593d0706ba00e9b13f74fc635a54f5
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-06 00:49:32 +0500

    Set up clarifia variables

--- client/src/App.js
+++ client/src/App.js
@@ -10,6 +10,45 @@ import {
   FaceRecognition,
 } from './components';
 
+// Your PAT (Personal Access Token) can be found in the portal under Authentification
+const PAT = process.env.REACT_APP_CLARIFAI_API_PAT;
+// Specify the correct user_id/app_id pairings
+// Since you're making inferences outside your app's scope
+const USER_ID =  process.env.REACT_APP_CLARIFAI_API_USER_ID;       
+const APP_ID =  process.env.REACT_APP_CLARIFAI_API_APP_ID;
+// Change these to whatever model and image URL you want to use
+const MODEL_ID = 'face-detection';  
+const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+
+///////////////////////////////////////////////////////////////////////////////////
+// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+///////////////////////////////////////////////////////////////////////////////////
+
+const raw = JSON.stringify({
+    "user_app_id": {
+        "user_id": USER_ID,
+        "app_id": APP_ID
+    },
+    "inputs": [
+        {
+            "data": {
+                "image": {
+                    "url": IMAGE_URL
+                }
+            }
+        }
+    ]
+});
+
+const requestOptions = {
+    method: 'POST',
+    headers: {
+        'Accept': 'application/json',
+        'Authorization': 'Key ' + PAT
+    },
+    body: raw
+};
+
 const initialState = {
   input: '',
 };
@@ -24,6 +63,8 @@ function App() {
   const onButtonSubmit = () => {
     console.log('clicked');
   };
+
+  console.log(APP_ID, USER_ID, PAT);
 
   return (
     <div className='App'>
