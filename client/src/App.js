efc89733ab999c20c6644ceca0ef1c6c43b5a62c
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-06 22:31:40 +0500

    Set up the clarifai api

--- client/src/App.js
+++ client/src/App.js
@@ -10,44 +10,40 @@ import {
   FaceRecognition,
 } from './components';
 
-// Your PAT (Personal Access Token) can be found in the portal under Authentification
-const PAT = process.env.REACT_APP_CLARIFAI_API_PAT;
-// Specify the correct user_id/app_id pairings
-// Since you're making inferences outside your app's scope
-const USER_ID =  process.env.REACT_APP_CLARIFAI_API_USER_ID;       
-const APP_ID =  process.env.REACT_APP_CLARIFAI_API_APP_ID;
-// Change these to whatever model and image URL you want to use
-const MODEL_ID = 'face-detection';  
-const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
+const returnClarifaiRequestOptions = (imageUrl) => {
+  const PAT = process.env.REACT_APP_CLARIFAI_PAT;
+  const USER_ID = process.env.REACT_APP_CLARIFAI_USER_ID;
+  const APP_ID = process.env.REACT_APP_CLARIFAI_APP_ID;
+  const MODEL_ID = 'face-detection';
+  // const IMAGE_URL = imageUrl;
+  const IMAGE_URL = 'https://samples.clarifai.com/metro-north.jpg';
 
-///////////////////////////////////////////////////////////////////////////////////
-// YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
-///////////////////////////////////////////////////////////////////////////////////
-
-const raw = JSON.stringify({
-    "user_app_id": {
-        "user_id": USER_ID,
-        "app_id": APP_ID
+  const raw = JSON.stringify({
+    user_app_id: {
+      user_id: USER_ID,
+      app_id: APP_ID,
     },
-    "inputs": [
-        {
-            "data": {
-                "image": {
-                    "url": IMAGE_URL
-                }
-            }
-        }
-    ]
-});
+    inputs: [
+      {
+        data: {
+          image: {
+            url: IMAGE_URL,
+          },
+        },
+      },
+    ],
+  });
 
-const requestOptions = {
+  return {
     method: 'POST',
     headers: {
-        'Accept': 'application/json',
-        'Authorization': 'Key ' + PAT
+      Accept: 'application/json',
+      Authorization: 'Key ' + PAT,
     },
-    body: raw
+    body: raw,
+  };
 };
+
 
 const initialState = {
   input: '',
@@ -62,9 +58,21 @@ function App() {
 
   const onButtonSubmit = () => {
     console.log('clicked');
+    fetch(
+      `https://api.clarifai.com/v2/models/face-detection/outputs`,
+      returnClarifaiRequestOptions(
+        'https://samples.clarifai.com/metro-north.jpg'
+      )
+    )
+      .then((response) => response.json())
+      .then((result) => console.log(result))
+      .catch((error) => console.log('error', error));
   };
 
-  console.log(APP_ID, USER_ID, PAT);
+  // console.log(process.env.REACT_APP_CLARIFAI_PAT,
+  //   process.env.REACT_APP_CLARIFAI_USER_ID,
+  //   process.env.REACT_APP_CLARIFAI_APP_ID,
+  //   process.env.REACT_APP_CLARIFAI_API_KEY);
 
   return (
     <div className='App'>
