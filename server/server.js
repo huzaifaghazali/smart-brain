29bac004ff8c4d5b0bd1a3f1173432a3c6d10cb4
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-10 21:48:43 +0500

    Create basic server setup

--- server/server.js
+++ server/server.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3001;
+
+app.get('/', (req, res) => {
+   res.send('This is working')
+})
+
+app.listen(port, () => {
+   console.log(`App is running on server ${port}`);
+})
+
+/* 
+
+/ --> res = this is working
+/signin --> POST = success/fail
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT --> user
+
+*/
