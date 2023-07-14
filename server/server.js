05b7b21db281ca3180624b0b6b5900bdce2d476f
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-14 23:52:23 +0500

    Added database in signin route

--- server/server.js
+++ server/server.js
@@ -23,43 +23,51 @@ app.use(express.json());
 // allows CORs policy
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: '123',
-      name: 'John',
-      email: 'john@gmail.com',
-      password: 'cookies',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: '124',
-      name: 'Sally',
-      email: 'sally@gmail.com',
-      password: 'bananas',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-};
-
 const port = process.env.PORT || 3001;
 
 app.get('/', (req, res) => {
   res.send(database.users);
 });
 
-app.post('/signin', (req, res) => {
+app.post('/signin', async (req, res) => {
   const { email, password } = req.body;
-  if (
-    email === database.users[0].email &&
-    password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(404).json('Error logging in');
-  }
+
+  postgresDB
+    .select('email', 'hash')
+    .from('login')
+    .where('email', '=', email)
+    .then((data) => {
+      const result = bcrypt.compareSync(password, data[0].hash);
+      if (result) {
+        return postgresDB
+          .select('*')
+          .from('users')
+          .where('email', '=', email)
+          .then((user) => {
+            res.json(user[0]);
+          })
+          .catch((err) => res.status(400).json('Unable to Login'));
+      } else {
+        res.status(400).json('Wrong Credentials');
+      }
+    })
+    .catch((err) => res.status(400).json('Wrong Credentials'));
+});
+
+app.get('/profile/:id', (req, res) => {
+  const { id } = req.params;
+  postgresDB
+    .select('*')
+    .from('users')
+    .where({
+      id,
+    })
+    .then((user) => {
+      if (user.length) {
+        res.json(user[0]);
+      }
+    })
+    .catch((err) => res.status(400).json('Not found'));
 });
 
 app.post('/register', async (req, res) => {
@@ -90,31 +98,16 @@ app.post('/register', async (req, res) => {
     .catch((err) => res.status(400).json('Unable to register'));
 });
 
-app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  postgresDB
-    .select('*')
-    .from('users')
-    .where({
-      id,
-    })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      }
-    })
-    .catch((err) => res.status(400).json('Not found'));
-});
-
 app.put('/image', (req, res) => {
   const { id } = req.body;
-  postgresDB('users').where('id', '=', id)
+  postgresDB('users')
+    .where('id', '=', id)
     .increment('entries', 1)
     .returning('entries')
-    .then(entries => {
+    .then((entries) => {
       res.json(entries[0].entries);
     })
-    .catch(err => res.status(400).json('unable to get entries'));
+    .catch((err) => res.status(400).json('unable to get entries'));
 });
 
 app.listen(port, () => {
