29838b61206637b93b4c10cc9ba7bd2fde6a4428
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-19 23:23:56 +0500

    Added comments

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -8,17 +8,20 @@ const handleRegister = async (req, res, postgresDB, bcrypt) => {
   try {
     const hash = await bcrypt.hash(password, 10);
 
+    //  Database transaction is a sequence of multiple operations performed on a database, 
     const loginEmail = await postgresDB.transaction(async (trx) => {
+      // Within the transaction, it first inserts the hashed password and email into the "login" table 
       const insertedEmail = await trx('login')
         .insert({
           hash: hash,
           email: email,
         })
-        .returning('email');
+        .returning('email'); // returns the email that was just inserted 
 
       return insertedEmail[0].email;
     });
 
+    // The value of loginEmail is then used as the email for the insertion into the "users" table, along with the name and the current date as the joined value. This information is inserted into the "users" 
     const user = await postgresDB('users').returning('*').insert({
       email: loginEmail,
       name: name,
