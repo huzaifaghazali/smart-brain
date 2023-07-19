dc1a36dca7665ae20d4a1028088e7285be20e5c0
Muhammad Huzaifa Ghazali <huzaifaghazali@gmail.com>
2023-07-19 23:05:36 +0500

    Refactored register controller

--- server/controllers/register.js
+++ server/controllers/register.js
@@ -1,34 +1,34 @@
 const handleRegister = async (req, res, postgresDB, bcrypt) => {
   const { name, email, password } = req.body;
+
   if (!email || !name || !password) {
     return res.status(400).json('Please fill out all the fields');
   }
-  const hash = await bcrypt.hash(password, (saltRounds = 10));
-  postgresDB
-    .transaction((trx) => {
-      trx
+
+  try {
+    const hash = await bcrypt.hash(password, 10);
+
+    const loginEmail = await postgresDB.transaction(async (trx) => {
+      const insertedEmail = await trx('login')
         .insert({
           hash: hash,
           email: email,
         })
-        .into('login')
-        .returning('email')
-        .then((loginEmail) => {
-          return postgresDB('users')
-            .returning('*')
-            .insert({
-              email: loginEmail[0].email,
-              name: name,
-              joined: new Date(),
-            })
-            .then((user) => {
-              res.json(user[0]);
-            });
-        })
-        .then(trx.commit)
-        .catch(trx.rollback);
-    })
-    .catch((err) => res.status(400).json('Unable to register'));
+        .returning('email');
+
+      return insertedEmail[0].email;
+    });
+
+    const user = await postgresDB('users').returning('*').insert({
+      email: loginEmail,
+      name: name,
+      joined: new Date(),
+    });
+
+    res.json(user[0]);
+  } catch (err) {
+    res.status(400).json('Unable to register');
+  }
 };
 
 module.exports = {
