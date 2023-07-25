2e3c6c34e18f6519a0dab566133bab7125eb7b38
huzaifaghazali <huzaifaghazali@gmail.com>
2023-07-26 00:07:27 +0500

    Added postgres in docker-compose

--- server/database/postgres.js
+++ server/database/postgres.js
@@ -1,14 +1,19 @@
 const knex = require('knex');
 
+// const postgresDB = knex({
+//    client: 'pg',
+//    connection: {
+//      host: process.env.DATABASE_HOST,
+//      port: process.env.DATABASE_PORT,
+//      user: process.env.DATABASE_USER,
+//      password: process.env.DATABASE_PASSWORD,
+//      database: 'smart-brain',
+//    },
+//  })
+
 const postgresDB = knex({
-   client: 'pg',
-   connection: {
-     host: process.env.DATABASE_HOST,
-     port: process.env.DATABASE_PORT,
-     user: process.env.DATABASE_USER,
-     password: process.env.DATABASE_PASSWORD,
-     database: 'smart-brain',
-   },
+  client: 'pg',
+  connection: process.env.POSTGRES_URL
  })
 
 
