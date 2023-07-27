6536d78508c35c49466aa4a9f440a6ca9ff87b27
huzaifaghazali <huzaifaghazali@gmail.com>
2023-07-27 19:34:04 +0500

    Added drop down button

--- client/src/components/Profile/ProfileIcon.jsx
+++ client/src/components/Profile/ProfileIcon.jsx
@@ -1,16 +1,30 @@
 import React, { useState } from 'react';
+import {
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from 'reactstrap';
+
 import PIcon from '../../assets/images/profileIcon1.svg';
 
 const ProfileIcon = () => {
   const [dropDownOpen, setDropDownOpen] = useState(false);
 
+  const toggle = () => setDropDownOpen((prevState) => !prevState);
+
   return (
     <div className='pa4 tc'>
-      <img
-        src={PIcon}
-        className='br-100 ba h3 w3 dib'
-        alt='avatar'
-      />
+      <Dropdown isOpen={dropDownOpen} toggle={toggle}>
+        <DropdownToggle data-toggle='dropdown' tag='span'>
+          <img src={PIcon} className='br-100 ba h3 w3 dib' alt='avatar' />
+        </DropdownToggle>
+        <DropdownMenu dark className='o-60 shadow-5'>
+          <DropdownItem>View Profile</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Signout</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
     </div>
   );
 };
