1aa0013371f287d63bab33479d1fdf97c79f93c4
huzaifaghazali <huzaifaghazali@gmail.com>
2023-07-28 19:58:49 +0500

    Added profile component

--- client/src/components/Profile/Profile.jsx
+++ client/src/components/Profile/Profile.jsx
@@ -1,11 +1,70 @@
 import React from 'react';
 import './Profile.css';
 
+import Logo from '../../assets/images/profileIcon.svg';
+
 const Profile = ({ isProfileOpen, toggleModal }) => {
   return (
-    <div className='profile-modal '>
-      Profile
-      <button onClick={toggleModal}>Click</button>
+    <div className='profile-modal'>
+      <article className='br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center bg-dark text-white o-80'>
+        <main className='pa4 white-80 w-80'>
+          <img src={Logo} className='h3 w3 dib' alt='avatar' />
+          <h1>Chris</h1>
+          <h4>{`Images submitted: 5`}</h4>
+          <p>{`Member since: date`}</p>
+          <hr />
+          <label className='mt2 fw6' htmlFor='user-name'>
+            Name:
+          </label>
+          <input
+            type='text'
+            name='user-name'
+            className='pa2 ba w-100 fw6'
+            placeholder='john'
+            id='name'
+          />
+          <label className='mt2 fw6' htmlFor='user-age'>
+            Age:
+          </label>
+          <input
+            type='text'
+            name='user-age'
+            className='pa2 ba w-100 fw6'
+            placeholder='age'
+            id='age'
+          />
+          <label className='mt2 fw6' htmlFor='user-age'>
+            Pet:
+          </label>
+          <input
+            type='text'
+            name='user-pet'
+            className='pa2 ba w-100 fw6'
+            placeholder='pet'
+            id='pet'
+          />
+          <div
+            className='mt4'
+            style={{ display: 'flex', justifyContent: 'space-evenly' }}
+          >
+            <button className='b pa2 grow pointer hover-white w-40 bg-info b--black-20'>
+              Save
+            </button>
+            <button
+              className='b pa2 grow pointer hover-white w-40 bg-danger b--black-20'
+              onClick={toggleModal}
+            >
+              Cancel
+            </button>
+          </div>
+        </main>
+        <div
+          className='modal-close'
+          onClick={toggleModal}
+        >
+          &times;
+        </div>
+      </article>
     </div>
   );
 };
